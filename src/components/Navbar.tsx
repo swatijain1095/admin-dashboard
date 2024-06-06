81e1adb16a8eb98b2d4a1d03f20b6cc22a1c8330
Swati Jain <jain.swati71@gmail.com>
2024-06-06 21:22:59 +0200

    Added sidebar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../img/logo.png";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -9,10 +17,25 @@ const Navbar = () => {
       <Link href="/">
         <Image src={logo} alt="Admin" width={40} />
       </Link>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback className="text-black">BT</AvatarFallback>
-      </Avatar>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback className="text-black">BT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
